993e506621364e165660e814875e8df6e9aa07b1
Graff <kamil.lesniak94@gmail.com>
2022-01-26 12:21:41 +0100

    fix devtool require

--- facades/Devtool/DevTool.php
+++ facades/Devtool/DevTool.php
@@ -7,7 +7,7 @@ class DevTool
 	public static function boot(): string
 	{
 		$time = round(microtime(true) - APP_START, 4);
-		require_once app_path('app/facades/devtool/index.php');
+		require_once __DIR__.'/index.php';
 
 		return ob_get_clean();
 	}
