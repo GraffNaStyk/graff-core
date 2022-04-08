8176dd5d1c6d5c59c4c81ab4741d72836f63379b
Graff <kamil.lesniak94@gmail.com>
2022-04-08 22:31:04 +0200

    add not empty json rule

---
+++ src/Validator/Rules/NotEmptyJson.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Facades\Validator\Rules;
+
+class NotEmptyJson extends Rule
+{
+	public function __construct(?string $description = null)
+	{
+		$this->description = $description ?? 'Must be a json';
+	}
+	
+	public function run(): bool
+	{
+		$field = json_decode($this->field);
+		
+		return ! empty($field);
+	}
+}
