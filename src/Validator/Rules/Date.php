5c063221832ee7fe680531039d0ecb7d2d405f48
Graff <kamil.lesniak94@gmail.com>
2022-04-30 11:28:40 +0200

    Fix Date validator

--- src/Validator/Rules/Date.php
+++ src/Validator/Rules/Date.php
@@ -9,12 +9,17 @@ class Date extends Rule
 	public function __construct(string $format = 'Y-m-d H:i:s', ?string $description = null)
 	{
 		$this->format      = $format;
-		$this->description = $description ?? 'Only letters';
+		$this->description = $description ?? 'Wrong date format';
 	}
 	
 	public function run(): bool
 	{
+		if ($this->field === null) {
+			return false;
+		}
+		
 		$dt = \DateTime::createFromFormat($this->format, $this->field);
+		
 		return $dt && $dt->format($this->format) === $this->field;
 	}
 }
