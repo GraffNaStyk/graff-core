c0ca0182ac23a96e3b9b906ae2fc50e9cbb713f5
Kamil <graffnastyk1994@gmail.com>
2022-05-31 09:46:37 +0200

    fix strtr replace "?"

--- src/Security/Sanitizer.php
+++ src/Security/Sanitizer.php
@@ -14,13 +14,13 @@ class Sanitizer
 		'/[\x{2600}-\x{26FF}]/u',
 		'/[\x{2700}-\x{27BF}]/u',
 	];
-	
+
 	public function clear(mixed $item): string|int|bool|array|null|float|object
 	{
 		if (! is_numeric($item)) {
 			$item = urldecode($item);
 		}
-		
+
 		if (Config::get('app.security.enabled')) {
 			$item = preg_replace('/<script\b[^>]*>(.*?)<\/script>/is', '', $item);
 			$item = preg_replace('/<noscript\b[^>]*>(.*?)<\/noscript>/is', '', $item);
@@ -31,26 +31,26 @@ class Sanitizer
 			$item = preg_replace('/<link (.*?)>/is', '', $item);
 			$item = preg_replace('/<video (.*?)>(.+)<\/video>/', '', $item);
 		}
-		
+
 		$item = strtr(
 			$item,
-			'???????��������������������������������������������������������������',
+			'��������������������������������������������������������������',
 			'SOZsozYYuAAAAAAACEEEEIIIIDNOOOOOOUUUUYsaaaaaaaceeeeiiiionoooooouuuuyy'
 		);
-		
+
 		$item = preg_replace('/(;|\||`|&|^|\)|\()/i', '', $item);
 		$item = preg_replace('/(\)|\(|\||&)/', '', $item);
 		$item = $this->cleanEmoji($item);
 
 		return Type::get($item);
 	}
-	
+
 	private function cleanEmoji(mixed $text): mixed
 	{
 		foreach ($this->regex as $regex) {
 			$text = preg_replace($regex, null, $text);
 		}
-		
+
 		return $text;
 	}
 }
