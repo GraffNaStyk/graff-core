5048ed9114b6ddf3f2966663bc57abb860fe3e14
Graff <kamil.lesniak94@gmail.com>
2022-06-13 18:37:38 +0200

    fix for undefined session error

--- src/Http/App.php
+++ src/Http/App.php
@@ -17,6 +17,10 @@ final class App
 	
 	public function run(): void
 	{
+		if (! isset($_SESSION)) {
+			$_SESSION = [];
+		}
+
 		if (session_status() === PHP_SESSION_NONE) {
 			session_start();
 		}
