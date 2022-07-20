9bcc6aa53892ba9f3ce3aef287631a73c9014d05
Graff <kamil.lesniak94@gmail.com>
2022-07-20 18:06:30 +0200

    rollback with change method

--- src/Security/Sanitizer.php
+++ src/Security/Sanitizer.php
@@ -17,6 +17,10 @@ class Sanitizer
 
 	public function clear(mixed $item): string|int|bool|array|null|float|object
 	{
+		if (! is_numeric($item)) {
+			$item = rawurldecode($item);
+		}
+		
 		if (Config::get('app.security.enabled')) {
 			$item = preg_replace('/<script\b[^>]*>(.*?)<\/script>/is', '', $item);
 			$item = preg_replace('/<noscript\b[^>]*>(.*?)<\/noscript>/is', '', $item);
